4e63fe6e1f0891f606ffb988c5887139bc4b13b6
Nicolas Borego <129863873+NBorego@users.noreply.github.com>
2023-08-25 11:45:49 -0300

    criação do match case

--- calculadora/main.py
+++ calculadora/main.py
@@ -5,9 +5,10 @@ sg.theme('reddit')
 #Variaveis
 fonte = ('Arial',14)
 tamanho_botao = (4,2)
+mensagem = []
 #layout
 layout = [
-    [sg.Multiline(size=(13,1), disabled=True, font=('Arial',25), no_scrollbar=True)],
+    [sg.Multiline('', size=(13,1), disabled=True, font=('Arial',25), no_scrollbar=True, key='tela')],
     [
         sg.Button('C', size=(tamanho_botao), font=(fonte)), 
         sg.Button('Apagar', size=(10,2), pad=3 ,font=(fonte)),
@@ -46,3 +47,10 @@ while True:
     eventos, valores = janela.read()
     if eventos == sg.WINDOW_CLOSED:
         break
+    if eventos == 'C':
+        janela['tela'].update('')
+    match eventos:
+        case '9':
+            mensagem.append('9')
+            janela['tela'].update(mensagem[-1], append=True)
+            print(f'{mensagem}')
